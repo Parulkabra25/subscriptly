d136128528bb482c50e4d5d5e3c34636598ed957
Parulkabra25 <parulkabra37@gmail.com>
2025-11-25 14:48:05 +0530

    Day 3: added DRF base setup and users API

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'books',
+    'rest_framework',
+    'users',
+    'subscriptions',
+    'payments',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL='users.User'
--- h
+++
@@ -1,3 +0,0 @@
-gui.recentrepo=C:/Users/Admin/hello
-user.name=Parulkabra25
-user.email=parulkabra37@gmail.com
